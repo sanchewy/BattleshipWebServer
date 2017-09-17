d55212f372ee87274c48c0e9f9a3b0d0e8475c7b
sanchewy <sanchewy@gmail.com>
2017-09-16 20:11:13 -0600

    This is the final version (#5)
    * Created a board, added battleship functionality
    
    * Cleaned up code for taking a hit
    
    * client/server functional in all client battleship attacks
    
    * Moved HTTP messages to after the header to fix http.client.BadStatusLine error. Also fixed the output of responses from client.
    
    * Fully functional
    
    * Delete opponent_board.txt
    
    * Delete server-battleship-methods.py
    
    * Delete own_board.html
    
    * Update server.py
    
    * Update client.py
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- client.py
+++ client.py
@@ -1,14 +1,63 @@
+#################################
+## CSCI-466: Networks          ##
+## Programming Assignment 1    ##
+## Battleship Across a Network ##
+## --------------------------- ##
+## Due 9/18/2017               ##
+## Authors:                    ##
+## Keinan Balsam               ##
+## Kincade Pavich              ##
+#################################
+
 import sys
 import http.client
+import re
+from pathlib import Path
 
 HOST_NAME = sys.argv[1]
 PORT_NUMBER = sys.argv[2]
 
 def send_fire_request(X_COOR, Y_COOR):
-    conn = http.client.HTTPConnection(HOST_NAME+":"+PORT_NUMBER)
-    conn.request("GET", "/x="+X_COOR+"&y="+Y_COOR)
-    r1 = conn.getresponse()
-    print(r1.status, r1.reason)
+    try:
+        conn = http.client.HTTPConnection(HOST_NAME+":"+PORT_NUMBER)
+        conn.request("GET", "/x="+X_COOR+"&y="+Y_COOR)
+        r1 = conn.getresponse()
+        result = r1.read()
+        print("Reason:{%s} Status:{%s} Message:{\n%s}" % (r1.reason, r1.status, result))
+        ## THIS SECTION UPDATES OUR KNOWLEDGE OF OPPONENT BOARD ##
+        if(r1.status == 200): #if shot was successful
+            boardFile = Path("opponent-board.txt") #get path to opponent board file
+            if boardFile.is_file(): #if it already exists
+                boardFile = open("opponent-board.txt") #open it and read it in
+                board = boardFile.read()
+                boardList = board.splitlines()
+
+                boardArray = [0] * 10
+                for i in range(len(boardArray)):
+                    boardArray[i] = [0] * 10
+                for i in range(len(boardArray)):
+                    for j in range(len(boardArray[i])):
+                        boardArray[i][j] = boardList[i][j]
+            else: # if file does not yet exist (first shot against opponent)
+                boardArray = [0] * 10 #populate all squares with underscores
+                for i in range(len(boardArray)):
+                    boardArray[i] = [0] * 10
+                for i in range(len(boardArray)):
+                    for j in range(len(boardArray[i])):
+                        boardArray[i][j] = "_"
+            if "1" in str(result): #if hit
+                boardArray[int(X_COOR)][int(Y_COOR)] = "X" #update with X
+            if "0" in str(result): #if miss
+                boardArray[int(X_COOR)][int(Y_COOR)] = "O" #update with O
+            fp = open("opponent-board.txt", "w") # update board file with new values
+            for i in range(len(boardArray)):
+                for j in range(len(boardArray[i])):
+                    fp.write(boardArray[i][j])
+                fp.write("\n");
+            fp.close()
+
+    except http.client.HTTPException as e:
+        print("Error:%s" %(e))
 
 if __name__ == '__main__':
     send_fire_request(sys.argv[3] ,sys.argv[4])
